454af63bfc9ad9632e1a0f467995a7df174d50fe
ran <colinyuran@126.com>
2018-02-04 19:49:35 +0800

    learn python basic

---
+++ LearnPythonBasic/helloworld.py
@@ -0,0 +1,13 @@
+#this is single line comment
+
+'''
+this is multiple line comment
+line 2
+line 3
+'''
+print('hello world \
+ a new line')
+
+print(r'this is a raw string \raw string')
+
+print(3/2)
