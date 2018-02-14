41a324d00fe187554d8d53741326ee467aeb4278
ran <colinyuran@126.com>
2018-02-14 23:01:57 +0800

    complete chapter 1

--- MLAction/kNN/kNN.py
+++ MLAction/kNN/kNN.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import operator
+from os import listdir
 
 def createDataSet():
     group = np.array([[1.0,1.1], [1.0,1.0], [0,0], [0,0.1]])
@@ -102,6 +103,49 @@ def classifyPerson(p=None, f=None, i=None):
     return None
 
 
+def img2vector(fileName):
+    returnVect = np.zeros((1,1024))
+    fr = open(fileName)
+
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32*i + j] = int(lineStr[j])
+
+    return returnVect
+
+
+def handWritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = np.zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s'%fileNameStr)
+    
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print('The classifier came back with:{0}, the real answer is:{1}'.format(classifierResult, classNumStr))
+
+        if (classifierResult != classNumStr):
+            errorCount += 1.0
+        
+    print('the total number of errors is:{0}'.format(errorCount))
+    print('the total error rate is:{0}'.format(errorCount / float(mTest)))
+    return None
+
+
 if (__name__ == '__main__'):
     # group, labels = createDataSet()
     # result = classify0([0,0], group, labels, 3)
@@ -118,4 +162,9 @@ if (__name__ == '__main__'):
 
     # datingClassTest()
 
-    classifyPerson(0.2, 1000, 10)
+    # classifyPerson(0.2, 1000, 10)
+
+    # testVector = img2vector('testDigits/0_13.txt')
+    # print(testVector)
+
+    handWritingClassTest()
