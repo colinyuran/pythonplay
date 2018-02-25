dd601a3a514cd01df61b3e868c4d200f1b541c01
喻燃 <ran@yurans-MacBook-Pro.local>
2018-02-25 10:24:32 +0800

    temp

--- LearnPythonBasic/helloworld.py
+++ LearnPythonBasic/helloworld.py
@@ -5,9 +5,47 @@ this is multiple line comment
 line 2
 line 3
 '''
-print('hello world \
- a new line')
+# print('hello world \
+#  a new line')
 
-print(r'this is a raw string \raw string')
+# print(r'this is a raw string \raw string')
 
-print(3/2)
+# print(3/2)
+
+# def fib(n):
+#     """
+#     this is a function to calculate fib
+#     """
+
+#     result = [0,1]
+#     for i in range(n-2):
+#         result.append(result[-1] + result[-2])
+#     return result
+
+# lst = fib(10)
+# print(lst)
+
+# print(fib.__doc__)
+
+# def foo(x,y,z, *args, **kargs):
+#     print(x)
+#     print(y)
+#     print(z)
+#     print(args)
+#     print(kargs)
+
+# foo(1,2,3)
+# foo(1,2,3,4,5,6)
+# foo(1,2,3,4,5,6,name='who', key='why')
+
+def foo(fun):
+    def wrap():
+        print("start")
+        fun()
+        print("end")
+        print(fun.__name__)
+    return wrap
+
+@foo
+def bar():
+    print("I am in bar()")
